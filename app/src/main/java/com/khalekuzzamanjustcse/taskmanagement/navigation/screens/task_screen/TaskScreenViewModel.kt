dc7668374bbf43b2452861f0e2f85b093a7161c1
Khalekuzzaman <74848657+khalekuzzamancse@users.noreply.github.com>
2023-10-23 17:42:13 +0600

    Task Screen ViewModel Updated:User fetched from database

--- app/src/main/java/com/khalekuzzamanjustcse/taskmanagement/navigation/screens/task_screen/TaskScreenViewModel.kt
+++ app/src/main/java/com/khalekuzzamanjustcse/taskmanagement/navigation/screens/task_screen/TaskScreenViewModel.kt
@@ -1,35 +1,59 @@
 package com.khalekuzzamanjustcse.taskmanagement.navigation.screens.task_screen
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.khalekuzzamanjustcse.taskmanagement.data.UserCollections
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class TaskScreenViewModel: ViewModel() {
+class TaskScreenViewModel : ViewModel() {
     //Manipulating the user selection and task
     //---------------
     private val _userSelectedMode = MutableStateFlow(false)
     val userSelectedMode = _userSelectedMode.asStateFlow()
     fun onUserSelectedModeChanged(show: Boolean) {
-        _userSelectedMode.value=show
+        _userSelectedMode.value = show
+    }
+
+    init {
+        viewModelScope.launch {
+                val newUser=UserCollections()
+                    .allUsers()
+                    .map {
+                        TaskAssignedUser(
+                            name = it.name,
+                            phone = it.phone,
+                            selected = false
+                        )
+                    }
+            withContext(Dispatchers.Main){
+                _users.value=newUser
+            }
+        }
     }
 
     //Manipulating the title input of tasks
     //---------------
     private val _title = MutableStateFlow("")
     val title = _title.asStateFlow()
-    fun onTitleChanged(title: String){
-        _title.value=title
+    fun onTitleChanged(title: String) {
+        _title.value = title
     }
+
     //Manipulating the description input
     //---------------
     private val _description = MutableStateFlow("")
     val description = _description.asStateFlow()
-    fun onDescriptionChanged(description: String){
-        _description.value=description
+    fun onDescriptionChanged(description: String) {
+        _description.value = description
     }
+
     //Manipulating the selected user
     //---------------
-    private val _users=MutableStateFlow(
+    private val _users = MutableStateFlow(
         listOf(
             TaskAssignedUser(name = "Mr Bean Karim", phone = "01738813861", selected = false),
             TaskAssignedUser(name = "Mr Bean Karim", phone = "01738813862", selected = false),
@@ -37,8 +61,8 @@ class TaskScreenViewModel: ViewModel() {
             TaskAssignedUser(name = "Mr Bean Karim", phone = "01738813864", selected = false),
         )
     )
-    val users=_users.asStateFlow()
-    fun onLongClick(index: Int){
+    val users = _users.asStateFlow()
+    fun onLongClick(index: Int) {
         val user = _users.value[index]
         val tmp = _users.value.map { it }.toMutableList()
         tmp.removeAt(index)
